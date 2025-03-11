6b0debc2210521bd8e8afc1176790bf1c28110f4
Ilyas Rehouni <ilyas.rehouni@gmail.com>
2025-03-11 10:45:45 +0100

    Himos cleaning

---
+++ Levi - Ylläs - Ruka - Himos/Himos_Cloud_Coverage/Himos_cleaning_2.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+input_file = r"Levi - Ylläs - Ruka - Himos\Himos_Cloud_Coverage\Himos_03_cleaned.csv"
+output_file = r"Levi - Ylläs - Ruka - Himos\Himos_Cloud_Coverage\Himos_03_cleaned_2.csv"
+
+with open(input_file, "r", encoding="utf-8") as f:
+    lines = f.readlines()
+
+cleaned_lines = []
+for line in lines:
+    line = line.strip().strip('"') 
+    line = line.replace('""', '"')
+    cleaned_lines.append(line + "\n") 
+
+with open(output_file, "w", encoding="utf-8") as f:
+    f.writelines(cleaned_lines)
